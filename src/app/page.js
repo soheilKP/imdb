5fd51afca5478bf41c44f27b395081119e04bf74
soheilKP <soheilkarimi79@gmail.com>
2023-09-10 13:22:17 +0330

    Fetch data from tmdb api in the home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,27 @@
-export default function Home() {
-  return <h1 className="text-orange-500">HOME</h1>;
+import Results from "@/components/Results";
+
+const API_KEY = process.env.API_KEY;
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+    }?api_key=${API_KEY}&language=en-US&page=1`,
+    { next: { revalidate: 10000 } }
+  );
+
+  const data = await res.json();
+
+  const results = data.results;
+
+  if (!res.ok) {
+    throw new Error("Fail to fetch data");
+  }
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
